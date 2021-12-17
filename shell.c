c23d9a9da6ae0909159383d5da99ad18e8fb5712
Lauren <guerralauren44@gmail.com>
2021-12-16 20:57:04 -0500

    lee espacios

--- shell.c
+++ shell.c
@@ -26,14 +26,6 @@ int SpecialCaracters(char* c)
     if(c==">>")return 1; 
     return 0;
 }
-
-void EjecuteLine(list* line)
-{
-
-}
-
-
-
 void ConcatChar(char c, char *chain)
 {
     char temp[2];
@@ -41,45 +33,86 @@ void ConcatChar(char c, char *chain)
     temp[1] = '\0';
     strcat(chain, temp);
 }
+char* GetCopy(char* s)
+{
+    char *copy = (char*)malloc(sizeof(char));//ver como le hago free
+    for(int i=0; s[i]!='\0'; i++)
+    {
+        ConcatChar(s[i],copy);
+    }
+   // free(copy);    
+    return copy;
+}
 
-int main()
+void EjecuteLine(list* line)
+{
+
+}
+
+int main(int argc, char const *argv[])
 {
      while (1)
     {
         printf("\n my-shell $ ");
 
+        char * word= (char*)malloc(sizeof(char));//word es cada una de las palabras que se mandan en un espacio de line
+        strcpy(word,""); 
 
-        //char word[0];  //word es cada una de las palabras que se mandan en un espacio de line
-        //word[0] = '\0';
-
-
-
-        char * word;//word es cada una de las palabras que se mandan en un espacio de line
-        bzero((char *)&word, sizeof(char));
-        list* line=init_list(""); //line es la linked list que guarda los comandos argumentos y caracteres especiales
-        
-
+        list* line=init_list("init"); //line es la linked list que guarda los comandos argumentos y caracteres especiales
+        pop_front(line);
 
         char c=getchar();  //cada uno de los char a leer de consola    
         while(c!= '\n')
         {
-            if(c=='#')//ya
+///////////////////////////////////////            
+            if(c=='#')
             {
                 break;
             }
-            if(c==';')//ya
+            if(c==';')
             {
                 ////////Primero terminar de armar la linea actual, y mandarla a ejecutar
-                push_back(line, word);
+                push_back(line, GetCopy(word));
+                //pop_front(line);
                 EjecuteLine(line);
+                print_list(line);                
+                pop_back(line);
+                c=getchar();
+                continue;
             }
-            if(SpecialCaracter(c))///pensar bien
+            if(SpecialCaracter(c))
             {            
-                if(word[0]=='\0')ConcatChar(c,word);//Si la palabra que tengo hasta el momento es vacia es porque se dejo un espacio y solo tengo que crear una nueva para el caracter especial
-                if(word[1]=='\0' && SpecialCaracter(word[0]))ConcatChar(c,word);//Si lo que tengo es un pedazo de caracter, lo completo
+                if(word[0]=='\0' ||SpecialCaracter(word[0]) )//Si la palabra que tengo hasta el 
+                //momento es vacia es porque se dejo un espacio y tengo que crear una nueva 
+                //para el caracter especial o en el caso que se este conforrmando un caracter especial,
+                //lo termino de conformar.
+                {
+                    ConcatChar(c,word);
+                    c=getchar();
+                    continue;
+                }
+            //     if(word[1]=='\0' && SpecialCaracter(word[0]))
+            //     {
+            //         ConcatChar(c,word);//Si lo que tengo es un pedazo de caracter, lo completo
+            //         c=getchar();
+            //         continue;
+            //     }
+                   push_back(line, GetCopy(word));
+                   strcpy(word,"");
+                   c=getchar();
+                   continue;      
                 
             }
 
+            if(SpecialCaracters(word))///Si es un caracter normal pero esta pegado a un caracter especial anterior
+            {
+                push_back(line, GetCopy(word));//saco el caracter especial para la lista y renuevo a word
+                strcpy(word,"");
+                c=getchar();
+                continue;
+                
+            }
+///////////////////////////////////////////////////////
             if(c==' ')
             {
                 if(word[0]=='\0')
@@ -88,43 +121,24 @@ int main()
                     continue;
                     
                 }//Si la palabra que tengo hasta el momento es vacia continuo(multiples espacios)
-                push_back(line, word);
-                strcpy(word,"");///vacio a word para comenzar una nueva palabra
-                //word[1];//
-                word[0]=c;
-
-
-
-            }
-
-
-            if(SpecialCaracters(word))///Si es un caracter normal pero esta pegado a un caracter especial anterior
-            {
-                push_back(line, word);//saco el caracter especial para la lista y renuevo a word
-                strcpy(word,"");
-                //word[1];
-                //word[0]='\0';
                 
+                push_back(line, GetCopy(word));
+                strcpy(word,"");///vacio a word para comenzar una nueva palabra
+                c=getchar();
+                continue;
             }
+            
             ConcatChar(c,word);
-
-
             c=getchar();
         }
-        push_back(line, word);////Primero poner la ultima palabra conformada y luego ejecutar la linea
-        
-        //print_list(line);
-        
-        printf("%d",line->head->value);
-
-
+    
+        push_back(line, GetCopy(word));////Primero poner la ultima palabra conformada y luego ejecutar la linea
+        //strcpy(word,"");
+        free(word);
+        //pop_front(line);
+        print_list(line);
         EjecuteLine(line);
-
-        //printf("%s",word);
-
-
-
-
+        free_list(line);
 
     }
 
@@ -132,7 +146,7 @@ int main()
 
 
  
-
+    return 0;
 }
 
 
