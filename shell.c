8a032e80df612cff8ad9857dfda7faec9bd55b0f
Lauren <guerralauren44@gmail.com>
2021-12-18 12:55:28 -0500

    Ya lee de consola

--- shell.c
+++ shell.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include "list.h"
+#include <stdbool.h>
 
+
+#pragma region Utiles
 
 int SpecialCaracter(char c)
 {
-    //if(c=='#')return 1;
-    //if(c==';')return 1;
     if(c=='&')return 1;
     if(c=='|')return 1;
     if(c=='<')return 1;
@@ -15,15 +16,13 @@ int SpecialCaracter(char c)
 }
 int SpecialCaracters(char* c)
 {
-    //if(c=='#')return 1;
-    //if(c==';')return 1;
-    if(c=="&")return 1;
-    if(c=="|")return 1;
-    if(c=="<")return 1;
-    if(c==">")return 1; 
-    if(c=="&&")return 1;
-    if(c=="||")return 1;
-    if(c==">>")return 1; 
+    if(strcmp(c,"&")==0)return 1;
+    if(strcmp(c,"|")==0)return 1;
+    if(strcmp(c,"<")==0)return 1;
+    if(strcmp(c,">")==0)return 1;
+    if(strcmp(c,"&&")==0)return 1;
+    if(strcmp(c,"||")==0)return 1;
+    if(strcmp(c,">>")==0)return 1;
     return 0;
 }
 void ConcatChar(char c, char *chain)
@@ -33,53 +32,64 @@ void ConcatChar(char c, char *chain)
     temp[1] = '\0';
     strcat(chain, temp);
 }
-char* GetCopy(char* s)
-{
-    char *copy = (char*)malloc(sizeof(char));//ver como le hago free
-    for(int i=0; s[i]!='\0'; i++)
-    {
-        ConcatChar(s[i],copy);
-    }
-   // free(copy);    
-    return copy;
-}
+
+#pragma endregion
 
 void EjecuteLine(list* line)
 {
 
 }
 
-int main(int argc, char const *argv[])
+void Shell()
 {
-     while (1)
+
+
+ while (1)
     {
         printf("\n my-shell $ ");
 
-        char * word= (char*)malloc(sizeof(char));//word es cada una de las palabras que se mandan en un espacio de line
+        char * word= (char*)calloc(sizeof(char),100);//word es cada una de las palabras que se mandan en un espacio de line
         strcpy(word,""); 
 
         list* line=init_list("init"); //line es la linked list que guarda los comandos argumentos y caracteres especiales
-        pop_front(line);
+        //pop_front(line);
 
-        char c=getchar();  //cada uno de los char a leer de consola    
+        char c=getchar();  //cada uno de los char a leer de consola   
+        
         while(c!= '\n')
         {
-///////////////////////////////////////            
+         
             if(c=='#')
             {
+                 while(c!= '\n')
+                 {
+                     c=getchar();
+                 }
                 break;
             }
+
+            if(c=='&')
+            {
+                /////mandar el proceso actual a background
+
+            }
+
             if(c==';')
             {
                 ////////Primero terminar de armar la linea actual, y mandarla a ejecutar
-                push_back(line, GetCopy(word));
-                //pop_front(line);
+                push_back(line, strdup(word));
+                strcpy(word,"");
+                pop_front(line);
                 EjecuteLine(line);
                 print_list(line);                
-                pop_back(line);
+                //pop_back(line);
+                free_list(line);
+                line=init_list("init");
+
                 c=getchar();
                 continue;
             }
+            
             if(SpecialCaracter(c))
             {            
                 if(word[0]=='\0' ||SpecialCaracter(word[0]) )//Si la palabra que tengo hasta el 
@@ -91,14 +101,9 @@ int main(int argc, char const *argv[])
                     c=getchar();
                     continue;
                 }
-            //     if(word[1]=='\0' && SpecialCaracter(word[0]))
-            //     {
-            //         ConcatChar(c,word);//Si lo que tengo es un pedazo de caracter, lo completo
-            //         c=getchar();
-            //         continue;
-            //     }
-                   push_back(line, GetCopy(word));
+                   push_back(line, strdup(word));
                    strcpy(word,"");
+                   ConcatChar(c,word);
                    c=getchar();
                    continue;      
                 
@@ -106,13 +111,14 @@ int main(int argc, char const *argv[])
 
             if(SpecialCaracters(word))///Si es un caracter normal pero esta pegado a un caracter especial anterior
             {
-                push_back(line, GetCopy(word));//saco el caracter especial para la lista y renuevo a word
+                push_back(line, strdup(word));//saco el caracter especial para la lista y renuevo a word
                 strcpy(word,"");
+                ConcatChar(c,word);
                 c=getchar();
                 continue;
                 
             }
-///////////////////////////////////////////////////////
+            
             if(c==' ')
             {
                 if(word[0]=='\0')
@@ -120,35 +126,31 @@ int main(int argc, char const *argv[])
                     c=getchar();
                     continue;
                     
-                }//Si la palabra que tengo hasta el momento es vacia continuo(multiples espacios)
-                
-                push_back(line, GetCopy(word));
+                }
+                push_back(line, strdup(word));
+
                 strcpy(word,"");///vacio a word para comenzar una nueva palabra
                 c=getchar();
                 continue;
             }
-            
             ConcatChar(c,word);
             c=getchar();
         }
-    
-        push_back(line, GetCopy(word));////Primero poner la ultima palabra conformada y luego ejecutar la linea
-        //strcpy(word,"");
+
+        push_back(line, strdup(word));
+        strcpy(word,"");
         free(word);
-        //pop_front(line);
+        pop_front(line);
         print_list(line);
-        EjecuteLine(line);
         free_list(line);
 
     }
 
 
-
-
- 
-    return 0;
 }
 
-
-
-
+int main(int argc, char const *argv[])
+{
+    Shell();
+    return 0;
+}
